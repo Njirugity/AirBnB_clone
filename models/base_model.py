70f291c5e86e733cea245064cecb9b5d204b8b07
brian <muriithibrian283@gmail.com>
2024-11-12 16:24:53 +0300

    Add class BaseModel that defines all common attributes/methods

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Defines the class BaseModel"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """The parent class for other classes. Describes the BaseModel"""
+    def __init__(self):
+        """Initializes instance attributes"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Updates the instance attribute updated_at"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of instance attributes"""
+        temp = self.__dict__.copy()
+        temp["__class__"] = self.__class__.__name__
+        temp["created_at"] = self.created_at.isoformat()
+        temp["updated_at"] = self.updated_at.isoformat()
+        return temp
+
+    def __str__(self):
+        """Returns a string representation"""
+        class_name = self.__class__.__name__
+        return (f"[{class_name}] ({self.id}) ({self.__dict__})")

6dde5461698db799c7b5caa0df5d21af271c8b94
brian <muriithibrian283@gmail.com>
2024-11-12 22:51:57 +0300

    Update file to recreate an instance for the dictionary represntation

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,23 @@ from datetime import datetime
 
 class BaseModel:
     """The parent class for other classes. Describes the BaseModel"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes instance attributes"""
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if kwargs:
+    	    for key, value in kwargs.items():
+                if key == __class__:
+                    continue
+                elif key == "created_at":
+                    self.created_at = datetime.strptime(kwargs[key], date_format)
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(kwargs[key], date_format)
+                else:
+                    self.id = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()

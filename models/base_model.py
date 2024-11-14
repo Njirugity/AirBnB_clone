8b415919e2a0ce2b45a7b8dc019d15313676ebfd
brian <muriithibrian283@gmail.com>
2024-11-14 20:37:41 +0300

    Update the file storage and create a new module user

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -29,10 +30,12 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        modele.storage.new(self)
 
     def save(self):
         """Updates the instance attribute updated_at"""
         self.updated_at = datetime.now()
+        model.storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of instance attributes"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Defines the class File Strorage for serializing and deserializing"""
+
+import json
+from models.base_model import BaseModel
+from models.user import User
+
+""" Defines the class File Storage"""
+
+
+class FileStorage:
+    """Represent the FileStorage class"""
+    __file_path = "file.json"
+    __object = {}
+
+    def all(self):
+        """Returns the dictionary __object"""
+        return FileStorage.__object
+
+    def new(self, obj):
+        """Stores a new object"""
+        key = (f"{__class__.__name__}.{obj.id}")
+        FileStorage.__object[key] = obj
+
+    def save(self):
+        """Serialize an object to a JSON file"""
+        obj_dict = {
+            key: value.to_dict()
+            for key, value in FileStorage.__object.items()
+        }
+        with open(FileStorage.__file_path, "w") as f:
+            json.dumps(obj_dict, f)
+
+    def reload(self):
+        """ Deserialize a JSON file"""
+        try:
+            obj_deserialized = {}
+            with open(FileStorage, "r") as f:
+                obj_deserialized = json.loads(f.read())
+            FileStorage.__objects = {
+                key: eval(val["__class__"])(**val)
+                for key, val in obj_deserialized.items()
+            }
+        except Exception:
+            pass

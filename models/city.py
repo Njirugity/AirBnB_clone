dc864baa834d85c3452a83c897978f53ea606ae3
brian <muriithibrian283@gmail.com>
2024-11-15 19:01:34 +0300

    Add user files and supporting classes. Updated file_storage

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines the class City"""
+from model.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represents the class City that inherits from BaseModel"""
+    state_id: str = ""
+    name: str = ""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,8 +4,12 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
-
-""" Defines the class File Storage"""
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from datetime import datetime
 
 
 class FileStorage:
